601be6308c7270a1c202c3ea8501f6a3b60440b4
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2022-12-21 12:25:46 +0100

    BullUpdate

---
+++ MazeGame87796/Assets/Scripts/NPC/Bull/Bull.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bull : MonoBehaviour
+{
+    public GameObject Player;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(Player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
