6a44973a2af1198d99a2e805d88085b21f6a5d19
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2023-01-09 14:36:04 +0100

    IK Bull Update

--- MazeGame87796/Assets/Scripts/NPC/Bull/Bull.cs
+++ MazeGame87796/Assets/Scripts/NPC/Bull/Bull.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class Bull : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject LookAtTarget;
     public NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -17,6 +18,8 @@ public class Bull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //agent.SetDestination(Player.transform.position);
+        Vector3 PlayerPos = Player.transform.position;
+        LookAtTarget.transform.position = new Vector3(PlayerPos.x, this.transform.position.y + 7.45f, PlayerPos.z);
+        //agent.SetDestination(PlayerPos);
     }
 }
